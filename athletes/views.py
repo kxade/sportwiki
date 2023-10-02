db8c6f738594171012acce00f90218a9a777cf3a
kxade <kxadecode@gmail.com>
2023-10-02 23:09:00 +0300

    add urls to posts

--- athletes/views.py
+++ athletes/views.py
@@ -2,31 +2,42 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpRespons
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
-menu = ["О сайте", "Добавить статью",  "Обратная связь", "Войти"]
+menu = [
+    {"title": "О сайте", "url_name": "about"},
+    {"title": "Добавить статью", "url_name": "add_page"},
+    {"title": "Обратная связь", "url_name": "contact"},
+    {"title": "Войти", "url_name": "login"},
+]
+
+db = [
+    {"id": 1, "title": "Хабиб Нурмагомедов", 'content': "Биография Хабиба Нурмагомедова", "is_published": True},
+    {"id": 2, "title": "Абдулрашид Саадулаев", 'content': "Биография Абдулрашида Саадулаева", "is_published": False},
+    {"id": 3, "title": "Ахмед Тажутдинов", 'content': "Биография Ахмеда Тажутдинова", "is_published": True},
+]
 
 
 def index(request):  #HttpRequest
-    data = {'title': 'Главная страница',
-            'menu': menu}
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'posts': db,
+    }
     return render(request, 'athletes/index.html', context=data)
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+def contact(request):
+    return HttpResponse(f"Обратная связь")
+
+def login(request):
+    return HttpResponse(f"Авторизация")
 
 def about(request):
     return render(request, "athletes/about.html", {'title': 'О сайте'})
 
-def categories(request, cat_id):  #HttpRequest
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
-
-def categories_by_slug(request, cat_slug):  #HttpRequest
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_slug}</p>")
-
-def archive(request, year):  #HttpRequest
-    if year > 2023:
-        uri = reverse('cats', args=("wrestlers",))
-        return HttpResponseRedirect(uri)
-    return HttpResponse(f"<h1>Архив по годам</h1><p>id: {year}</p>")
-
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
