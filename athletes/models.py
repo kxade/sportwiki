90327a32a45fe9124063e607c9d9df738ea8c40a
kxade <kxadecode@gmail.com>
2023-10-15 20:18:05 +0300

    add a new model for tagging and sorting posts by tags

--- athletes/models.py
+++ athletes/models.py
@@ -30,6 +30,7 @@ class Athlete(models.Model):
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name="posts")
+    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags')
 
     objects = models.Manager()
     published = PublishedManager()
@@ -46,3 +47,14 @@ class Athlete(models.Model):
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
+
+
+class TagPost(models.Model):
+    tag = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=250, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.tag
+
+    def get_absolute_url(self):
+        return reverse('tag', kwargs={'tag_slug': self.slug})
