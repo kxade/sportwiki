de2f75a4fea82625c382a34b082f4c07032df467
kxade <kxadecode@gmail.com>
2023-11-26 20:59:12 +0300

    add changing status feature in admin panel

--- athletes/models.py
+++ athletes/models.py
@@ -7,8 +7,12 @@ class PublishedManager(models.Manager):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
     slug = models.SlugField(max_length=150, unique=True, db_index=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 
     def __str__(self):
         return self.name
@@ -23,15 +27,17 @@ class Athlete(models.Model):
         PUBLISHED = 1, 'Опубликовано'
 
 
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name="posts")
-    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags')
-    medcard = models.OneToOneField('MedCard', on_delete=models.SET_NULL, null=True, blank=True, related_name="athlete")
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="Slug")
+    content = models.TextField(blank=True, verbose_name="Текст статьи")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Последнее изменение")
+    is_published = models.BooleanField(choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
+                                       default=Status.DRAFT, verbose_name="Статус")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name="posts", verbose_name="Категория")
+    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags', verbose_name="")
+    medcard = models.OneToOneField('MedCard', on_delete=models.SET_NULL, null=True, blank=True,
+                                   related_name="athlete", verbose_name="Медкарта")
 
     objects = models.Manager()
     published = PublishedManager()
