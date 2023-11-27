218a483f64219b5bf46d7ccbb6aa6a27a78e629d
kxade <kxadecode@gmail.com>
2023-11-27 23:11:36 +0300

    add a column for brief_info

--- athletes/admin.py
+++ athletes/admin.py
@@ -3,12 +3,15 @@ from .models import Athlete, Category
 
 @admin.register(Athlete)
 class AthleteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'is_published', 'cat')
-    list_display_links = ('id', 'title')
+    list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
+    list_display_links = ('title',)
     ordering = ('time_create', 'title')
     list_editable = ('is_published',)
     list_per_page = 5
 
+    @admin.display(description="Краткое описание", ordering="content")
+    def brief_info(self, athlete: Athlete):
+        return f"Описание {len(athlete.content)} символов"
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
