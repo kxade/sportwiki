5298b6323e11860f915f1f550f26454acac21dc1
kxade <kxadecode@gmail.com>
2023-12-13 01:24:17 +0300

    link the unrelated form to the db

--- athletes/views.py
+++ athletes/views.py
@@ -27,7 +27,11 @@ def addpage(request):
     if request.method == 'POST':
         form = AddPostForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            try:
+                Athlete.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, "Ошибка добавления поста")
     else:
         form = AddPostForm()
 
