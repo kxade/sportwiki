e97245e35daa56688d9ee4cd211c89a30172af7a
kxade <kxadecode@gmail.com>
2023-12-13 23:50:41 +0300

    add custom validation to a form field

--- athletes/forms.py
+++ athletes/forms.py
@@ -1,13 +1,48 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.utils.deconstruct import deconstructible
+
 from .models import Category, MedCard
 
 
+@deconstructible
+class RussianValidator:
+    ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЧЦШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхчцшщьыъэюя0123456789- "
+    code = 'russian'
+
+    def __init__(self, message=None):
+        self.message = message if message else "Должны присутствовать только русские символы, дефис и пробел."
+
+    def __call__(self, value, *args, **kwargs):
+        if not (set(value) <= set(self.ALLOWED_CHARS)):
+            raise ValidationError(self.message, code=self.code)
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label="Заголовок")
-    slug = forms.SlugField(max_length=255, label="URL")
+    title = forms.CharField(max_length=255, min_length=5,
+                            label="Заголовок",
+                            validators = [
+                                RussianValidator(),
+                            ],
+                            error_messages={
+                                'min_length': "Слишком короткий заголовок",
+                                'required': "Без заголовка никак",
+                            })
+    slug = forms.SlugField(max_length=255, label="URL",
+                           validators=[
+                               MinLengthValidator(5, message="Минимум 5 символов"),
+                               MaxLengthValidator(100, message="Максимум 100 символов")
+                           ])
     content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 4}), required=False, label="Контент")
     is_published = forms.BooleanField(required=False, initial=True, label="Статус")
     cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Категория не выбрана", label="Категория")
     medcard = forms.ModelChoiceField(queryset=MedCard.objects.all(), required=False, empty_label="Мед. карта не выбрана", label="Мед. карта")
 
+
+    #при валидации единичного поля
+    # def clean_title(self):
+    #     title = self.cleaned_data['title']
+    #     ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЧЦШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхчцшщьыъэюя0123456789- "
+    #
+    #     if not (set(title) <= set(ALLOWED_CHARS)):
+    #         raise ValidationError("Должны присутствовать только русские символы, дефис и пробел.")
 
