16e1ef3430dbc07bfef662651a8ab3a3d6e53c74
kxade <kxadecode@gmail.com>
2023-12-24 04:45:45 +0300

    show loaded images in posts and admin panel

--- athletes/admin.py
+++ athletes/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin, messages
+from django.utils.safestring import mark_safe
+
 from .models import Athlete, Category
 
 
@@ -20,13 +22,13 @@ class MedCardFilter(admin.SimpleListFilter):
 
 @admin.register(Athlete)
 class AthleteAdmin(admin.ModelAdmin):
-    fields = ('title', 'slug', 'content', 'cat', 'tags')
+    fields = ('title', 'slug', 'content', 'photo', 'post_photo', 'cat', 'tags')
    #exclude = вариант для исключения полей
-   #readonly_fields = поля только для чтения
+    readonly_fields = ['post_photo']#поля только для чтения
     prepopulated_fields = {'slug': ('title',)}
     filter_horizontal = ('tags',)
    #filter_vertical = вертикальный вид выбора
-    list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
+    list_display = ('title', 'post_photo', 'time_create', 'is_published', 'cat')
     list_display_links = ('title',)
     ordering = ('time_create', 'title')
     list_editable = ('is_published',)
@@ -34,10 +36,13 @@ class AthleteAdmin(admin.ModelAdmin):
     actions = ('set_published', 'set_draft')
     search_fields = ('title', 'cat__name')
     list_filter = (MedCardFilter, 'cat__name', 'is_published')
+    save_on_top = True
 
-    @admin.display(description="Краткое описание", ordering="content")
-    def brief_info(self, athlete: Athlete):
-        return f"Описание {len(athlete.content)} символов"
+    @admin.display(description="Изображение", ordering="content")
+    def post_photo(self, athlete: Athlete):
+        if athlete.photo:
+            return mark_safe(f'<img src="{athlete.photo.url}" "width=50">')
+        return "Без фото"
 
     @admin.action(description="Опубликовать выбранные записи")
     def set_published(self, request, queryset):
--- sportwiki/urls.py
+++ sportwiki/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from athletes.views import page_not_found
+from sportwiki import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +27,9 @@ urlpatterns = [
 ]
 
 handler404 = page_not_found
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = 'Панель управления'
 admin.site.index_title = 'Дагестанские спортсмены'
