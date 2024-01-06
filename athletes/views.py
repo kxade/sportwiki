41986d186d1e13a14059827b7484ce7548a3a3e9
kxade <kxadecode@gmail.com>
2024-01-07 00:24:04 +0300

    remove outdated code in views

--- athletes/views.py
+++ athletes/views.py
@@ -10,19 +10,6 @@ from uuid import uuid4
 
 from .utils import DataMixin
 
-menu = {}
-
-# def index(request):  #HttpRequest
-#     posts = Athlete.published.all().select_related('cat')
-#
-#     data = {
-#         'title': 'Главная страница',
-#         'menu': menu,
-#         'posts': posts,
-#         'cat_selected': 0,
-#     }
-#     return render(request, 'athletes/index.html', context=data)
-
 
 class AthleteHome(DataMixin, ListView):
     template_name = 'athletes/index.html'
@@ -32,37 +19,6 @@ class AthleteHome(DataMixin, ListView):
 
     def get_queryset(self):
         return Athlete.published.all().select_related('cat')
-
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['title'] = "Главная страница"
-    #     context['menu'] = menu
-    #     context['posts'] = Athlete.published.all().select_related('cat')
-    #     context['cat_selected'] = int(self.request.GET.get('cat_id', 0))
-    #     return context
-
-
-# def addpage(request):
-#     if request.method == 'POST':
-#         form = AddPostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             # try:
-#             #     Athlete.objects.create(**form.cleaned_data)
-#             #     return redirect('home')
-#             # except:
-#             #     form.add_error(None, "Ошибка добавления поста")
-#             form.save() # доступно при использовании формы связанной с моделью
-#             return redirect('home')
-#     else:
-#         form = AddPostForm()
-#
-#     data = {'menu': menu,
-#             'title': 'Добавление статьи',
-#             'form': form
-#
-#     }
-#     return render(request, 'athletes/addpage.html', context=data)
 
 
 class AddPage(CreateView):
@@ -76,6 +32,7 @@ class AddPage(CreateView):
         'title': 'Добавление статьи',
     }
 
+
 class UpdatePage(UpdateView):
     model = Athlete
     fields = ['title', 'content', 'photo', 'is_published', 'cat']
@@ -87,42 +44,11 @@ class UpdatePage(UpdateView):
     }
 
 
-# class AddPage(View):
-#     def get(self, request):
-#         form = AddPostForm()
-#         data = {'menu': menu,
-#                 'title': 'Добавление статьи',
-#                 'form': form
-#                 }
-#         return render(request, 'athletes/addpage.html', context=data)
-#
-#     def post(self, request):
-#         form = AddPostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('home')
-#
-#         data = {'menu': menu,
-#                 'title': 'Добавление статьи',
-#                 'form': form
-#         }
-#         return render(request, 'athletes/addpage.html', context=data)
-
-
 def contact(request):
     return HttpResponse(f"Обратная связь")
 
 def login(request):
     return HttpResponse(f"Авторизация")
-
-
-# функция для загрузки файлов без привязки к модели
-# def handle_uploaded_file(f):
-#     with open(f"uploads/{f.name}{uuid4}", "wb+") as destination:
-#         for chunk in f.chunks():
-#             destination.write(chunk)
-
-
 
 def about(request):
     if request.method == "POST":
@@ -134,21 +60,9 @@ def about(request):
     else:
         form = UploadFileForm()
 
-
-    return render(request, "athletes/about.html", {'title': 'О сайте', 'menu': menu,
-                                                   'form': form})
-
-# def show_post(request, post_slug):
-#
-#     post = get_object_or_404(Athlete, slug=post_slug)
-#
-#     data = {
-#         'title': post.title,
-#         'menu': menu,
-#         'post': post,
-#         'cat_selected': 1,
-#     }
-#     return render(request, 'athletes/post.html', data)
+    return render(request,
+                  "athletes/about.html",
+                  {'title': 'О сайте', 'menu': menu, 'form': form})
 
 
 class ShowPost(DataMixin, DetailView):
@@ -224,3 +138,8 @@ def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
 
 
+# функция для загрузки файлов без привязки к модели
+# def handle_uploaded_file(f):
+#     with open(f"uploads/{f.name}{uuid4}", "wb+") as destination:
+#         for chunk in f.chunks():
+#             destination.write(chunk)
